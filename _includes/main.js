0f8c591c1fc32721f3fdfa27e1214d75a453ba86
Oleksandr Lapchenko <ozeron@me.com>
2017-02-12 01:25:22 +0200

    Updates

---
+++ _includes/main.js
@@ -0,0 +1,16 @@
+window.QUIZES = {
+  'default': "<div data-fsid='e.589f8953090c6f010096c1bc' data-width='100%' data-height='auto'></div>"
+};
+
+$('#quiz').ready(function(){
+  var root = $('#quiz');
+  var script = document.createElement('script');
+  var query = Url.parseQuery();
+  var quiz = QUIZES[query.quiz] || QUIZES['default'];
+  var div = $.parseHTML(quiz);
+
+  $(script).attr('src', 'https://4screens.net/4screens-service-loader.js');
+  $(script).prop('async');
+  root.append(div);
+  root.append(script);
+});
