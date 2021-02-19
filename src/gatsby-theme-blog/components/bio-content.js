02f0885f9e626c30866f3162b416b27f4d02638a
ozeron <ozeron@me.com>
2021-02-19 13:12:32 +0200

    WIP: improved bio

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -8,9 +8,14 @@ import { Styled } from "theme-ui"
 export default function Bio() {
   return (
     <>
-      Hello, I'm <Styled.a href="https://twitter.com/ethernalnow">Alex Lapchenko</Styled.a>
+      Hello, I'm <Styled.a href="https://twitter.com/hey_iam_alex">Alex Lapchenko</Styled.a>
       <br />
       I blog about products and productivity.
+      <br />
+      <h2>Projects</h2>
+      <ul>
+        <li><Styled.a href="http://twible.alexlapchenko.com/">Twible – distraction free writing experience</Styled.a></li>
+      </ul>
     </>
   )
 }
