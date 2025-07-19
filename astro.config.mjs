05d1c16aa4b6297cf6e8393de610e71364103245
Alex Lapchenko <alex.toptal@opslevel.com>
2025-07-19 18:56:00 +0100

    feat: initialize blog template with dark mode and markdown support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
+import remarkGfm from 'remark-gfm'
+import remarkSmartypants from 'remark-smartypants'
+import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alexlapchenko.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://astro-blog-template.netlify.app',
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
+})
